7ac11d78d111c2efd9ea1a5327eb84e92709d82f
Matteo Giannone <matteogiannone24@gmail.com>
2023-06-07 12:22:12 -0400

    Added anova tables

--- src/Components/AnovaTableSelector/OneWayAnovaTable/index.tsx
+++ src/Components/AnovaTableSelector/OneWayAnovaTable/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import './style.css';
+import { table } from 'console';
 
 function OneWayAnovaTable() {
     return (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", fontSize: "50px", fontWeight: "bold", margin: "1em"}}>One-Way Anova</div>
+        <table>
+            <tr style={{height: "2em"}}>
+                <th style={{width: "25%"}}>Source of Variation</th>
+                <th style={{width: "15%"}}>df</th>
+                <th style={{width: "20%"}}>SS</th>
+                <th style={{width: "20%"}}>MS</th>
+                <th style={{width: "20%"}}>F</th>
+            </tr>
+            <tr style={{height: "3em"}}>
+                <th>Treatment/Between</th>
+                <td>a-1</td>
+                <td>SSA</td>
+                <td>MSA</td>
+                <td style={{borderBottom: "0"}}>MSA/MSE</td>
+            </tr>
+            <tr style={{height: "3em"}}>
+                <th>Error/Within</th>
+                <td>N-a</td>
+                <td>SSE</td>
+                <td>MSE</td>
+                <td style={{borderTop: "0"}}></td>
+            </tr>
+            <tr style={{height: "2em"}}>
+                <th>Total</th>
+                <td>N-1</td>
+                <td>SST</td>
+            </tr>
+        </table>
     );   
 }
 
--- src/Components/AnovaTableSelector/TwoWayAnovaTable/index.tsx
+++ src/Components/AnovaTableSelector/TwoWayAnovaTable/index.tsx
@@ -3,7 +3,48 @@ import './style.css';
 
 function TwoWayAnovaTable() {
     return (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", fontSize: "50px", fontWeight: "bold", margin: "1em"}}>Two-Way Anova</div>
+        <table>
+            <tr style={{height: "2em"}}>
+                <th style={{width: "25%"}}>Source of Variation</th>
+                <th style={{width: "15%"}}>df</th>
+                <th style={{width: "20%"}}>SS</th>
+                <th style={{width: "20%"}}>MS</th>
+                <th style={{width: "20%"}}>F</th>
+            </tr>
+            <tr style={{height: "2em"}}>
+                <th>Factor A</th>
+                <td>a-1</td>
+                <td>SSA</td>
+                <td>MSA</td>
+                <td style={{borderBottom: "0"}}>MSA/MSE</td>
+            </tr>
+            <tr style={{height: "2em"}}>
+                <th>Factor B</th>
+                <td>b-1</td>
+                <td>SSB</td>
+                <td>MSB</td>
+                <td style={{borderBottom: "0"}}>MSB/MSE</td>
+            </tr>
+            <tr style={{height: "2em"}}>
+                <th>Interaction</th>
+                <td>(a-1)(b-1)</td>
+                <td>SSAB</td>
+                <td>MSAB</td>
+                <td>MSAB/MSE</td>
+            </tr>
+            <tr style={{height: "3em"}}>
+                <th>Error</th>
+                <td>N-a</td>
+                <td>SSE</td>
+                <td>MSE</td>
+                <td style={{border: "0"}}></td>
+            </tr>
+            <tr style={{height: "2em"}}>
+                <th>Total</th>
+                <td>N-1</td>
+                <td>SST</td>
+            </tr>
+        </table>
     );   
 }
 
