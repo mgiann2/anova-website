7b4c1bc0b671919226d329dd710660c88def9dba
Matteo Giannone <matteogiannone24@gmail.com>
2023-06-07 11:25:20 -0400

    Added functionality to AnovaTableSelector

--- src/Components/AnovaTableSelector/TwoWayAnovaTable/index.tsx
+++ src/Components/AnovaTableSelector/TwoWayAnovaTable/index.tsx
@@ -3,7 +3,7 @@ import './style.css';
 
 function TwoWayAnovaTable() {
     return (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", fontSize: "50px", fontWeight: "bold", margin: "1em"}}>One-Way Anova</div>
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center", fontSize: "50px", fontWeight: "bold", margin: "1em"}}>Two-Way Anova</div>
     );   
 }
 
--- src/Components/AnovaTableSelector/index.tsx
+++ src/Components/AnovaTableSelector/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import OneWayAnovaTable from './OneWayAnovaTable';
 import TwoWayAnovaTable from './TwoWayAnovaTable';
 
+enum TableState {
+    OneWayAnova, 
+    TwoWayAnova
+}
+
 function AnovaTableSelector() {
+    let [currTable, updateCurrTable] = useState(TableState.OneWayAnova);
+
+    function renderTable() {
+        switch(currTable) {
+            case TableState.OneWayAnova:
+                return (<OneWayAnovaTable />);
+            case TableState.TwoWayAnova:
+                return (<TwoWayAnovaTable />);
+        }
+    }
+
     return (
         <div className='grid-container'>
-            <button className='selector-btn-selected'>One-Way Anova</button>
-            <button className='selector-btn'>Two-Way Anova</button>
+            <button className={currTable === TableState.OneWayAnova ? 'selector-btn-selected' : 'selector-btn'} onClick={() => updateCurrTable(TableState.OneWayAnova)}>One-Way Anova</button>
+            <button className={currTable === TableState.TwoWayAnova ? 'selector-btn-selected' : 'selector-btn'} onClick={() => updateCurrTable(TableState.TwoWayAnova)}>Two-Way Anova</button>
             <div className='anova-content'>
-                <OneWayAnovaTable />
+                { renderTable() }
             </div>
         </div>
     );
